6207e6da01cde9379cb7730243350f38b0af83ac
MaxU1301 <34748240+MaxU1301@users.noreply.github.com>
2021-06-12 23:41:32 -0400

    Add files via upload

---
+++ Debian GNOME App Install.sh
@@ -0,0 +1,29 @@
+# install all apps by Max
+sudo apt update
+sudo apt upgrade
+sudo apt install apt-transport-https curl -y
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install git -y
+sudo apt install putty -y
+sudo apt install menulibre -y
+sudo apt install gimp -y
+sudo apt install gnome-tweaks -y
+sudo apt install CubicSDR -y
+sudo apt install brave-browser -y
+
+
+sudo flatpak install flathub com.visualstudio.code -y # VSCode
+sudo flatpak install flathub com.spotify.Client -y # Spotify
+sudo flatpak install flathub org.signal.Signal -y # Signal
+sudo flatpak install flathub us.zoom.Zoom -y # Zoom
+sudo flatpak install flathub com.discordapp.Discord -y # Discord
+sudo flatpak install flathub com.bitwarden.desktop -y # Bitwarden
+sudo flatpak install flathub org.audacityteam.Audacity -y # Audacity
+
+sudo apt update
+sudo apt upgrade
