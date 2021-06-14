1c3bd05ff4a1fb7539f386326195adb21db952d6
max@ullrichfamily.com <max@pop-os.localdomain>
2021-06-14 14:05:34 -0400

    added folder color for nautilus, added flatpak repo

--- Debian GNOME App Install.sh
+++ Debian GNOME App Install.sh
@@ -1,11 +1,18 @@
 # install all apps by Max
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install apt-transport-https curl -y
+sudo apt install flatpak -y
+
+# Repos
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
+sudo add-apt-repository ppa:costales/folder-color
+
+# Apt install
 sudo apt update
 
 sudo apt install git -y
@@ -15,8 +22,9 @@ sudo apt install gimp -y
 sudo apt install gnome-tweaks -y
 sudo apt install cubicsdr -y
 sudo apt install brave-browser -y
+sudo apt install folder-color
 
-
+# Flatpak install
 sudo flatpak install flathub com.visualstudio.code -y # VSCode
 sudo flatpak install flathub com.spotify.Client -y # Spotify
 sudo flatpak install flathub org.signal.Signal -y # Signal
@@ -26,16 +34,18 @@ sudo flatpak install flathub com.bitwarden.desktop -y # Bitwarden
 sudo flatpak install flathub org.audacityteam.Audacity -y # Audacity
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
+nautilus -q
+
+# Custom Launchers
 sudo mkdir /opt/appstartup
 sudo cp App\ Startup/CubicSDR/CubicSDR.sh /opt/appstartup/CubicSDR.sh
 sudo cp App\ Startup/CubicSDR/CubicSDR.desktop /usr/share/applications
 
+# Nvidia Settings
 printf 'nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"' > ~/nvidia-settings.sh
 chmod +x ~/nvidia-settings.sh
-
-
 
 # RTL-SDR
 sudo apt update -y
